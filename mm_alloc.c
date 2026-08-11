bc09ed8a6b1690b56287efbd976a26bbc235165b
Nico Crevier <nwcrevier@gmail.com>
2026-08-11 13:45:55 -0400

    mm_malloc is implemented except for the splitting of freeblocks that are significantly larger than what is asked for by the caller of mm_malloc. That will be done soon.

--- mm_alloc.c
+++ mm_alloc.c
@@ -3,7 +3,6 @@
  */
 
 #include "mm_alloc.h"
-#include "internal_mm_alloc.h"
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -12,6 +11,7 @@
 #include <string.h>
 #include <stddef.h>
 
+#define ALIGNMENT 8
 /*
  * Basic Program Structure: 
  * Initialize a default (empty) free list on malloc call 
@@ -31,20 +31,56 @@ typedef (is32bit ? uint8_t : uint16_t) Align;
 
 
 typedef struct free_list free_list;
-struct free_list { 
-	size_t size;
-	free_list* prev;
-	free_list* next;
-	bool free;
-	alignas(max_align_t) unsigned char mem_block[];
+struct free_list {
+    size_t header;  // size of the entire block including header and footer (i.e. adding size to a pointer to the block will be the pointer to the next block) 
+                    // the LSB of header is whether or not it is allocated, the next LSB is whether or not the previous block in memory is allocated
+    union {
+        struct { free_list* prev; free_list* next; } links;
+        unsigned char mem_block[];
+    } body;
 };
+
+static size_t calculate_size(free_list* free_block) {
+    return (free_block->header & ~3);
+}
+
+static inline void set_footer(free_list* free_block) {
+    *((size_t *)((unsigned char*)free_block + (calculate_size(free_block)-sizeof(size_t)))) = calculate_size(free_block); // replaces the last sizeof(size_t) bytes with the size of the free_list
+}
+static inline void set_previous_allocated(free_list* free_block, bool previous_allocated) {
+    free_block->header = ((free_block->header & ~(1 << 1)) | (previous_allocated << 1));
+}
+static inline void set_allocated(free_list* free_block, bool allocated) {
+    free_block->header = ((free_block->header & ~1) | allocated);
+}
+
+static void setup_free_block(free_list* free_block, size_t total_size, bool allocated, bool previous_allocated) {
+    free_block->header = total_size;
+
+    set_allocated(free_block, allocated);
+    set_previous_allocated(free_block, previous_allocated);
+    
+    if(!allocated) 
+       set_footer(free_block); 
+
+    return;
+}
+
+
+static inline bool is_allocated(free_list* free_block) {
+    return (free_block->header & (size_t)1);
+}
+
+static inline bool previous_is_allocated(free_list* free_block) {
+    return (free_block->header & (size_t)(1 << 1));
+}
 
 static free_list* program_free_list;
 static bool free_list_is_initialized = false;
 
 static void* grow_heap(size_t size) {
     void* heap_ptr;
-    if ((heap_ptr = sbrk((size + 31) & ~(size_t)31) == (void *)-1) { // forces size to the next greatest multiple of 32 (unless it is already a mult of 32)
+    if ((heap_ptr = sbrk(size)) == (void *)-1) { // forces size to the next greatest multiple of 32 (unless it is already a mult of 32)
         perror("Error No Memory");
         return nullptr;
     }
@@ -52,29 +88,10 @@ static void* grow_heap(size_t size) {
     return heap_ptr;
 }
 
-static void* new_free_block(size_t size) {
-    void* heap_ptr = grow_heap(size);
-
-    
-}
-
-static void init_free_list_values(free_list* free_l) {
-    free_l->prev = nullptr;
-    free_l->next = nullptr;
-    free_l->free = false;
-}
-
 free_list* create_and_insert_free_block(size_t size) {
-    free_list* heap_ptr = grow_heap(sizeof(free_list) + size);
-    *heap_ptr = (free_list) {
-        .prev = nullptr,
-        .next = program_free_list,
-        .size = size,
-        .free = true,
-    };
-
-    program_free_list->prev = heap_ptr;
-
+    free_list* heap_ptr = grow_heap(size);
+    setup_free_block(heap_ptr, size, true, false); //previous_allocated is false because *prev is nullptr for the first entry in the list
+    
     return heap_ptr;
 }
 
@@ -82,37 +99,42 @@ static void check_and_split(free_list* current_free_block, size_t size) {// chec
     return;
 }
 
+static void remove_from_free_list(free_list* free_block) {
+    (free_block->body.links.prev)->body.links.next = free_block->body.links.next;
+    (free_block->body.links.next)->body.links.prev = free_block->body.links.prev;
+    
+    set_allocated(free_block, true);
+}
+
 static free_list* find_block(free_list* current_free_block, size_t size) {
-    if (current_free_block->free && current_free_block->size <= size) {
-        check_and_split(current_free_block, size); 
+    if (calculate_size(current_free_block) >= size) {
+        check_and_split(current_free_block, size);
+        remove_from_free_list(current_free_block);
         return current_free_block;
-    } else if ((current_free_block->next)) {
-        return find_block(current_free_block->next, size);
+    } else if ((current_free_block->body.links.next)) {
+        return find_block(current_free_block->body.links.next, size);
     } else {
-        return create_and_insert_free_block(size);
+        return create_and_insert_free_block(size); // this needs to use setup_free_block
     }
 }
 
 void* mm_malloc(size_t size) {
+    size_t total_size = ALIGNMENT + sizeof(free_list) + ((size + 31) & ~(size_t)31);
+    free_list* chosen_block; 
+    
     if (!free_list_is_initialized) {
-        if (!(program_free_list = (free_list*) grow_heap(sizeof(free_list) + size))) {
+        if (!(chosen_block = grow_heap(total_size) + ALIGNMENT)) {
             perror("Error Upon Initializing Free List");
             exit(1);
         }
-        init_free_list_values(program_free_list);
-        program_free_list->size = size;
+        setup_free_block(chosen_block, total_size, true, false);
         
         free_list_is_initialized = true;
-        
-        return (void *) (program_free_list->mem_block);
     } else {
         // search for location
-        free_list* chosen_block = find_block(program_free_list, size);
-        chosen_block->free = false;
-
-        return (void *) (chosen_block->mem_block);
+        free_list* chosen_block = find_block(program_free_list, total_size);
     }
-  return nullptr;
+    return (void *)(chosen_block->body.mem_block);
 }
 
 void* mm_calloc(size_t size) {
